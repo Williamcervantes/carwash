9dfff09e891a0e5a707cabe1f0fafb299d0d2ea9
Pedro William Cunha <pcunha@clubegazetadopovo.com.br>
2021-01-09 22:26:03 -0300

    agora vai

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,13 +3,12 @@ import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
 
-
 function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>SHANGRILÁ VEÍCULOS</h1>
-      <p>What are you waiting for?</p>
+      <p>Limpeza e higiene automotiva</p>
       <div className='hero-btns'>
         <Button
           className='btns'

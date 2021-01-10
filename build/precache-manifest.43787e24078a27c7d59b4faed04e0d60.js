4e3d2f814b5cfe7525a10258db95806d435c04d4
Pedro William Cunha <pcunha@clubegazetadopovo.com.br>
2021-01-09 22:32:06 -0300

    sending changes

---
+++ build/precache-manifest.43787e24078a27c7d59b4faed04e0d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c1babaa73e904e259d6ffcaa445d0e",
+    "url": "/carwash/index.html"
+  },
+  {
+    "revision": "f1ee07b1ab6b820b92ac",
+    "url": "/carwash/static/css/main.73329814.chunk.css"
+  },
+  {
+    "revision": "cddfd08082f2f28533ae",
+    "url": "/carwash/static/js/2.126bf728.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/carwash/static/js/2.126bf728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ee07b1ab6b820b92ac",
+    "url": "/carwash/static/js/main.dcc11b9f.chunk.js"
+  },
+  {
+    "revision": "b5b817f2ff8faa488584",
+    "url": "/carwash/static/js/runtime-main.c4499f2d.js"
+  }
+]);
